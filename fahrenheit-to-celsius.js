7b1ba6780bb7a1cb755ac60802f535327f24505b
aknathweb <aniknwu.cse@gmail.com>
2022-08-06 10:27:30 +0600

    celsius and fahreheit temperature convertion

---
+++ fahrenheit-to-celsius.js
@@ -0,0 +1,7 @@
+// ২. একইভাবে উল্টা হিসাব করবে। যাতে তোমাকে ফারেনহাইট হিসেবে তাপমাত্রা দিলে সেটাকে সেলসিয়াস এ কনভার্ট করে আউটপুট দিবে। 
+
+function FahrenheitToCelsius(Fahrenheit) {
+    const Celsius = (5 / 9) * (Fahrenheit - 32);
+    return Celsius;
+}
+console.log("Fahrenheit to Celsius temperature : ", FahrenheitToCelsius(68));
